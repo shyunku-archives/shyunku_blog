b19d07dcb22d985ccc8cd84cbc02c99e3d490e62
whdudgns412 <whdudgns7321@gmail.com>
2019-05-28 18:20:48 +0900

    0.6.3 update

--- shyunku/settings.py
+++ shyunku/settings.py
@@ -52,7 +52,7 @@ USER_INFO_CONDITION = {
 
 PROCESS_RATE = '83'
 SITE_VER = '0.6.3v beta'
-CSS_VER = 138
+CSS_VER = 137
 
 PAGE_MAX_DETAILS = 15       # 게시글 한 페이지당 보여지는 포스팅 수
 PAGE_MAX_CHAPTER = 10       # 게시판 페이지가 한번에 보여지는 수
