a9d836fb8f6335d5e73e65e5d76eb97989542a5d
whdudgns412 <whdudgns7321@gmail.com>
2019-05-29 02:15:16 +0900

    manage.py modified

--- manage.py
+++ manage.py
@@ -4,18 +4,18 @@ import os
 import sys
 
 
-def main():
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shyunku.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        )
+    except ImportError:
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
