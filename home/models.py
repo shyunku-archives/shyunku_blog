8b095e7310cf4e72cfa96acf6e24c3c143d3be10
whdudgns412 <whdudgns7321@gmail.com>
2019-05-29 02:25:49 +0900

    ckeditor deleted

--- home/models.py
+++ home/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from ckeditor.fields import RichTextField
 from django_summernote import models as summer_model
 from django_summernote import fields as summer_fields
 
